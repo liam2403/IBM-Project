240e11bcc7e8cbba31188a2af47b0af0fd5e3bd7
unknown <jacobjstraw@gmail.com>
2023-02-22 15:01:35 +0000

    Add firebase voilerplate

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB84MzTaUpIFBf1Lc4Gm5nMcvvJjU5vg8s",
+  authDomain: "ibm-education-app.firebaseapp.com",
+  databaseURL: "https://ibm-education-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ibm-education-app",
+  storageBucket: "ibm-education-app.appspot.com",
+  messagingSenderId: "241018868660",
+  appId: "1:241018868660:web:28446bb2812ac94e77d8e1",
+  measurementId: "G-QQ7MFLR0TT"
+};
+
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
